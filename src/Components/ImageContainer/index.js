4bf3b1241343608f39dae539dc982b477d8b1505
zerochae <zerochae@kakao.com>
2022-02-02 17:25:52 +0900

    Add: 모달(Tooltip) 기본 레이아웃 설정

--- src/Components/ImageContainer/index.js
+++ src/Components/ImageContainer/index.js
@@ -2,49 +2,76 @@ import React from "react";
 
 import * as S from "./style";
 
+const detail = (
+  <img
+    src="//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png"
+    alt="detail"
+  />
+);
+
+const close = (
+  <img
+    src="//cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png"
+    alt="close"
+  />
+);
+
+const arrow = (
+  <img
+    className="btnArrow"
+    src="//cdn.ggumim.co.kr/storage/20211102181936xqHzyWAmb8.png"
+    alt="arrow"
+  />
+);
+
 const ImageContainer = (props) => {
-  const detail = (
-    <img
-      src="//cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png"
-      alt="detail"
-    />
-  );
-
-  const close = (
-    <img
-      src="//cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png"
-      alt="close"
-    />
-  );
-
   const handleSearch = (index) => {
     props.selected !== index ? props.setSelected(index) : props.setSelected();
-  }
+  };
 
   return (
     <S.IMGContainer>
-      <S.Img imageUrl={props.data.imageUrl}>
+      <S.Img imageUrl={props.data.imageUrl} onClick={()=>{}}>
         {props.data &&
           props.data.productList.map((item, index) => {
             return (
-              <S.Btn
-                key={item.productId}
-                X={item.pointX}
-                Y={item.pointY}
-                onClick={()=>{handleSearch(index)}}
-              >
-                {props.selected !== index ? detail : close}
-              </S.Btn>
+              <S.BtnContainer X={item.pointX} Y={item.pointY}>
+                <S.Btn
+                  key={item.productId}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleSearch(index);
+                  }}
+                >
+                  {props.selected !== index ? detail : close}
+                </S.Btn>
+                {props.selected === index ? <Modal data={item} /> : null}
+              </S.BtnContainer>
             );
           })}
-        <Modal />
       </S.Img>
     </S.IMGContainer>
   );
 };
 
-const Modal = () => {
-  return <S.ModalContainer></S.ModalContainer>;
+const Modal = (props) => {
+  return (
+    <S.ModalContainer>
+      <S.ModalImg imageUrl={props.data.imageUrl}></S.ModalImg>
+      <S.ModalContent>
+        <h4 className="productName">{props.data.productName}</h4>
+        <p>
+          {props.data.discountRate !== 0 ? (
+            <span className="discountRate">{props.data.discountRate}%</span>
+          ) : (
+            <span className="noDiscount">예상가</span>
+          )}
+          <span className="price">{props.data.priceDiscount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</span>
+        </p>
+      </S.ModalContent>
+        {arrow}
+    </S.ModalContainer>
+  );
 };
 
 export default ImageContainer;
--- src/Components/ImageContainer/style.js
+++ src/Components/ImageContainer/style.js
@@ -14,18 +14,81 @@ export const Img = styled.div`
   position: relative;
 `;
 
-export const ModalContainer = styled.div``;
+export const BtnContainer = styled.div`
+  top: ${(props) => props.X * 1.6}px;
+  left: ${(props) => props.Y * 1.66}px;
+  position: absolute;
+`;
 
 export const Btn = styled.div`
-width: 40px;
-height: 40px;
-position: absolute;
-cursor: pointer;
-top: ${props => props.X * 1.66}px;
-left: ${props => props.Y * 1.66}px;
+  width: 40px;
+  height: 40px;
+  z-index: 5;
+  cursor: pointer;
 
-img{
-  width: 32px;
-  height: 32px;
-}
-`
+  img {
+    width: 32px;
+    height: 32px;
+  }
+`;
+export const ModalContainer = styled.div`
+  width: 220px;
+  height: 86px;
+  position: relative;
+  background: #fffffff2;
+  padding: 8px 0 8px 8px;
+  margin-top: 16px;
+  border-radius: 7px;
+  z-index: 999;
+  display: flex;
+
+  & .btnArrow {
+    height: 20px;
+    width: 20px;
+    align-self: end;
+  }
+`;
+
+export const ModalImg = styled.div`
+  background: url(${(props) => props.imageUrl});
+  width: 70px;
+  height: 70px;
+  background-size: cover;
+  background-position: center;
+  margin: 0 8px 0 0;
+  border-radius: 4px;
+`;
+
+export const ModalContent = styled.div`
+  flex:1;
+  padding: 0 0 2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+
+  & .productName {
+    color: #333c45;
+    line-height: 14px;
+    text-overflow: ellipsis;
+  }
+
+  & .discountRate {
+    color:#ff585d;
+    margin: 0 4px 0 0;
+    font-weight: bold;
+  }
+
+  & .noDiscount {
+    color: #898f94;
+    font-size: 11px;
+    font-weight: bold;
+    margin: 0 4px 0 0;
+  }
+
+  & .price {
+    color:#181d1f;
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
